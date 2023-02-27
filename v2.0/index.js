c0f34a71e9b32b7d0b509d76336a196303b1d042
complex robot <107013508+complexrobotworks@users.noreply.github.com>
2023-02-27 15:36:45 -0500

    First upload
    This is a bit of a long time coming, but I was fighting with a design concept and I think I'm off to a solid start. There's no broken code, I did lift and shift some of the cr1.0 stuff since it's still applicable, just some moderate re-tooling. Figure now is as good a time as any to commit to the repo so that if god forbid I suffer some kind of catastrophic event with my laptop all isn't lost.

---
+++ v2.0/index.js
@@ -0,0 +1,64 @@
+const colTheme = [
+    {
+        bg: '#ACDFFA'
+    },
+    {
+        bg: '#FFB501'
+    },
+    {
+        bg: '#FFAFA7'
+    },
+    {
+        bg: '#BCD089'
+    }
+];
+
+// Old theme below, this can be removed later...
+// const colTheme = [
+//     {
+//         bg: '#A0DBF2',
+//         txt: '#000000'
+//     },
+//     {
+//         bg: '#4EA68D',
+//         txt: '#FFFFFF'
+//     },
+//     {
+//         bg: '#A60505',
+//         txt: '#FFFFFF'
+//     },
+//     {
+//         bg: '#D93B3B',
+//         txt: '#FFFFFF'
+//     }
+// ];
+
+function colSel() {
+    return Math.floor(Math.random() * 4);
+}
+
+function footerDate() {
+    document.getElementById('footDate').textContent = new Date().getFullYear();
+};
+
+function setElementColors() {
+    let rnd = colSel();
+    document.getElementById('offCanvasBody').style.backgroundColor = colTheme[rnd].bg;
+    // The below are legacy from 1.0
+    // document.getElementById('helpdiv').style.color = colTheme[rnd].txt;
+    // document.getElementById('helpdiv').style.backgroundColor = colTheme[rnd].bg;
+    // document.getElementById('helpnotlimit').style.color = colTheme[rnd].txt;
+    // document.getElementById('helpnotlimit').style.backgroundColor = colTheme[rnd].bg;
+    // document.getElementById('webport').style.color = colTheme[rnd].txt;
+    // document.getElementById('webport').style.backgroundColor = colTheme[rnd].bg;
+    // document.getElementById('artport').style.color = colTheme[rnd].txt;
+    // document.getElementById('artport').style.backgroundColor = colTheme[rnd].bg;
+    // document.getElementById('logoport').style.color = colTheme[rnd].txt;
+    // document.getElementById('logoport').style.backgroundColor = colTheme[rnd].bg;
+    // document.getElementById('building').style.color = '#D2D2D2';
+    // document.getElementById('band').style.color = '#000000';
+    // document.getElementById('portfolio').style.color = colTheme[1].bg;
+    // document.getElementById('logos').style.color = '#A60505';
+};
+
+setInterval(cycleGearCol, 1000);
